ab4ae7341345d15db45aeef4178e8536acb9e072
makia <g1821120@gmail.com>
2019-12-02 01:33:48 +0900

    風船を追加中

--- Game/Game/src/Result.hpp
+++ Game/Game/src/Result.hpp
@@ -7,15 +7,17 @@
 
 #include"Common.hpp"
 
-class Result : public MyApp::Scene
-{
+class Result : public MyApp::Scene {
 private:
-    
+	Array<Texture> m_balloons;
+	Array<Duration> m_balloonsPos;
+	Array<double> m_posXs;
+	Array<Vec2> m_posYs;
 public:
-    
-    Result(const InitData& init);
-    
-    void update() override;
-    
-    void draw() const override;
+
+	Result (const InitData& init);
+
+	void update () override;
+
+	void draw () const override;
 };
