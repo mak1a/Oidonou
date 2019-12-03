3b1df9ae7c17b6ec23fb59e670c2de1be516c141
makia <g1821120@gmail.com>
2019-12-03 20:39:09 +0900

    コメントを追加

--- Game/Game/src/Title.cpp
+++ Game/Game/src/Title.cpp
@@ -1,54 +1,48 @@
 
 # include "Title.hpp"
 
-Title::Title(const InitData& init)
-	: IScene(init)
-{
+Title::Title (const InitData& init)
+	: IScene (init) {
 
 }
 
-void Title::update()
-{
-	m_startTransition.update(m_startButton.mouseOver());
-	m_exitTransition.update(m_exitButton.mouseOver());
+void Title::update () {
+	m_startTransition.update (m_startButton.mouseOver ());
+	m_exitTransition.update (m_exitButton.mouseOver ());
 
-	if (m_startButton.mouseOver() || m_exitButton.mouseOver())
-	{
-		Cursor::RequestStyle(CursorStyle::Hand);
+	if (m_startButton.mouseOver () || m_exitButton.mouseOver ()) {
+		Cursor::RequestStyle (CursorStyle::Hand);
 	}
 
-	if (m_startButton.leftClicked())
-	{
-		changeScene(State::Game);
+	if (m_startButton.leftClicked ()) {
+		changeScene (State::Game);
 	}
 
-	if (m_exitButton.leftClicked())
-	{
-		System::Exit();
+	if (m_exitButton.leftClicked ()) {
+		System::Exit ();
 	}
 }
 
-void Title::draw() const
-{
+void Title::draw () const {
 	const String titleText = U"おいどのうゲーム";
-	const Vec2 center(Scene::Center().x, 120);
-    
-    TextureAsset(U"River").resized(1400, 830).drawAt(Scene::CenterF());
-    
-    TextureAsset(U"Schoolkun").resized(400.f, 300.f).drawAt(Vec2(1230.f, Scene::CenterF().movedBy(0, 120).y).movedBy(-100.f, -100.f));
-    
-	FontAsset(U"Title")(titleText).drawAt(center.movedBy(4, 6), ColorF(0.0, 0.5));
-	FontAsset(U"Title")(titleText).drawAt(center);
+	const Vec2 center (Scene::Center ().x, 120);
 
-	m_startButton.draw(ColorF(1.0, m_startTransition.value())).drawFrame(2);
-	m_exitButton.draw(ColorF(1.0, m_exitTransition.value())).drawFrame(2);
+	TextureAsset (U"River").resized (1400, 830).drawAt (Scene::CenterF ());
 
-	FontAsset(U"Menu")(U"はじめる").drawAt(m_startButton.center(), ColorF(0.25));
-	FontAsset(U"Menu")(U"おわる").drawAt(m_exitButton.center(), ColorF(0.25));
+	TextureAsset (U"Schoolkun").resized (400.f, 300.f).drawAt (Vec2 (1230.f, Scene::CenterF ().movedBy (0, 120).y).movedBy (-100.f, -100.f));
 
-	Rect(0, 500, Scene::Width(), Scene::Height() - 500)
-		.draw(Arg::top = ColorF(0.0, 0.0), Arg::bottom = ColorF(0.0, 0.5));
+	FontAsset (U"Title")(titleText).drawAt (center.movedBy (4, 6), ColorF (0.0, 0.5));
+	FontAsset (U"Title")(titleText).drawAt (center);
 
-	const int32 highScore = getData().highScore;
-	FontAsset(U"Score")(U"前回のscore: {}"_fmt(highScore)).drawAt(Vec2(620, 550));
+	m_startButton.draw (ColorF (1.0, m_startTransition.value ())).drawFrame (2);
+	m_exitButton.draw (ColorF (1.0, m_exitTransition.value ())).drawFrame (2);
+
+	FontAsset (U"Menu")(U"はじめる").drawAt (m_startButton.center (), ColorF (0.25));
+	FontAsset (U"Menu")(U"おわる").drawAt (m_exitButton.center (), ColorF (0.25));
+
+	Rect (0, 500, Scene::Width (), Scene::Height () - 500)
+		.draw (Arg::top = ColorF (0.0, 0.0), Arg::bottom = ColorF (0.0, 0.5));
+
+	const int32 highScore = getData ().score;
+	FontAsset (U"Score")(U"前回のscore: {}"_fmt (highScore)).drawAt (Vec2 (620, 550));
 }
